8570b5b01d7860cddd753f75540b4c22dda8789c
Jack <91816314+Jakirsayyed98@users.noreply.github.com>
2025-09-25 18:41:53 +0530

    Create get.go

---
+++ module/notification/get.go
@@ -0,0 +1,41 @@
+package notification
+
+import (
+	"PerkHub/stores"
+	"PerkHub/utils"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetAdminNotificationList(c *gin.Context) {
+	store, err := stores.GetStores(c)
+	if err != nil {
+		utils.RespondBadRequest(c, err, "")
+		return
+	}
+
+	result, err := store.NotificationStore.GetAdminNotificationList()
+	if err != nil {
+		utils.RespondBadRequest(c, err, "")
+		return
+	}
+
+	utils.RespondOK(c, result, "Successfully get notification list", "")
+}
+
+func SendNotification(c *gin.Context) {
+	id := c.Param("id")
+	store, err := stores.GetStores(c)
+	if err != nil {
+		utils.RespondBadRequest(c, err, "")
+		return
+	}
+
+	result, err := store.NotificationStore.AdminSendNotification(id)
+	if err != nil {
+		utils.RespondBadRequest(c, err, "")
+		return
+	}
+
+	utils.RespondOK(c, result, "Notification send successfullly", "")
+}
